b17a1ac9e1ae17cc624d0bd4c183ee608c40fab3
abetoluwani <101352514+abetoluwani@users.noreply.github.com>
2023-09-29 08:18:28 +0000

    little change

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+import '../modules/signin/bindings/signin_binding.dart';
+import '../modules/signin/views/signin_view.dart';
+import '../modules/splashscreen/bindings/splashscreen_binding.dart';
+import '../modules/splashscreen/views/splashscreen_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.SPLASHSCREEN;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.SIGNIN,
+      page: () => const SigninView(),
+      binding: SigninBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASHSCREEN,
+      page: () => const SplashscreenView(),
+      binding: SplashscreenBinding(),
+    ),
+  ];
+}
---
+++ lib/app/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const primary = Color(0XFF8B3E00);
+  static const white = Color(0XFFFFFFFF);
+  static const black = Color(0XFF00000F);
+  static const primarylight = Color(0XFFFFD6B3);
+  static const red = Color(0XFFFD2727);
+  
+}
