a361aeb48e7f2a40e20d363cc029f83f7fd8325e
abetoluwani <101352514+abetoluwani@users.noreply.github.com>
2023-10-07 17:32:06 +0000

    forgotten password

--- lib/app/modules/forgot_password/views/forgot_password_view.dart
+++ lib/app/modules/forgot_password/views/forgot_password_view.dart
@@ -1,10 +1,13 @@
 import 'package:flaury/app/theme/colors.dart';
 import 'package:flaury/app/widgets/apptext.dart';
+import 'package:flaury/app/widgets/fgbutton.dart';
 import 'package:flaury/app/widgets/space.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 import '../controllers/forgot_password_controller.dart';
 
@@ -13,32 +16,68 @@ class ForgotPasswordView extends GetView<ForgotPasswordController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: true,
-          leading: Icon(
-            Icons.navigate_before,
-            size: 50.h,
-            color: AppColors.black,
-          ),
-          title: BigAppText('Forgot Password'),
-          centerTitle: false,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        leading: Icon(
+          Icons.navigate_before,
+          size: 50.h,
+          color: AppColors.black,
         ),
-        body: Container(
-          padding: simPad(20, 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: Image.asset(
-                  'assets/images/forgot_pass.png',
-                  height: 250.h,
-                ),
+        title: BigAppText('Forgot Password'),
+        centerTitle: false,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        padding: simPad(20, 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                'assets/images/forgot_pass.png',
+                height: 250.h,
               ),
-            ],
-          ),
-        ),);
+            ),
+            vSpace(30),
+            MedAppText(
+              'Select how you would like to reset your password',
+              fontSize: 20,
+            ),
+            vSpace(50),
+            GestureDetector(
+              onTap: () {
+                // Handle tap gesture here (e.g., navigate to another page)
+                if (kDebugMode) {
+                  print('SMS container tapped!');
+                }
+              },
+              child: const FgButton(
+                bordercolor: AppColors.white,
+                hint: 'Via SMS',
+                label: '+2348072****',
+                icon: Iconsax.message,
+              ),
+            ),
+            vSpace(30),
+            GestureDetector(
+              onTap: () {
+                // Handle tap gesture here (e.g., navigate to another page)
+                if (kDebugMode) {
+                  print('Email container tapped!');
+                }
+              },
+              child: const FgButton(
+                bordercolor: AppColors.white,
+                hint: 'Via Email',
+                label: 'bec***om@mail.com',
+                icon: Icons.email_rounded,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
